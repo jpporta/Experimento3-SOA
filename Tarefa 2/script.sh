cf56f64c5e09eeba7ec0900f9226d89f184d9758
Joao Porta <jpedro.porta@gmail.com>
2018-04-17 19:23:39 +0000

    Testes Feitos

--- Tarefa 2/script.sh
+++ Tarefa 2/script.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-gcc t2.c -o tc -D PROTECT
-sudo ./t2
+gcc t2.c -o t2 -D PROTECT
+
+for x in [1..5]
+do
+	sudo ./t2 >> resProtected.txt
+done
+
+gcc t2.c -o t2
+
+for x in [1..5]
+do
+	sudo ./t2 >> resNOTprotected.txt
+done
+rm t2
+
